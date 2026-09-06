5d15a1b56b009d97b8376b45bc64af8af6c521d5
janczesko12 <janczesko12@gmail.com>
2026-09-06 14:03:24 +0200

    Initialize Firebase on iOS

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -1,8 +1,13 @@
 import SwiftUI
+import FirebaseCore
 import ComposeApp
 
 @main
 struct iosApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
